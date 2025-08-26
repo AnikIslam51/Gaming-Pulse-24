75082aec5c1c8c76052b9115531d253b015dd91e
AnikIslam51 <anik.elahi@gmail.com>
2025-08-26 15:45:32 +0600

    updated documet.js

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="canonical"
+          href="https://gamingpulse24.com/"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
